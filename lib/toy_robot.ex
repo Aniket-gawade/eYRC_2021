4300d8bb013a94b435477995e8058d87dcb4c937
Aniket-gawade <aniket.gawade@spit.ac.in>
2021-11-03 13:22:42 +0530

    toy_robot updated

--- lib/toy_robot.ex
+++ lib/toy_robot.ex
@@ -5,15 +5,14 @@ defmodule ToyRobot do
   @table_top_y :e
   # mapping of y-coordinates
   @robot_map_y_atom_to_num %{:a => 1, :b => 2, :c => 3, :d => 4, :e => 5}
+  @directions_to_the_right %{north: :east, east: :south, south: :west, west: :north}
 
   defstruct x: 1, y: 1, facing: :north, walkable: true, gcost: 0,hcost: 10,parent: Elixir.ToyRobot
 
 
   @doc """
   Places the robot to the default position of (1, A, North)
-
   Examples:
-
       iex> ToyRobot.place
       {:ok, %ToyRobot.Position{facing: :north, x: 1, y: :a}}
   """
@@ -35,15 +34,11 @@ defmodule ToyRobot do
   @doc """
   Places the robot to the provided position of (x, y, facing),
   but prevents it to be placed outside of the table and facing invalid direction.
-
   Examples:
-
       iex> ToyRobot.place(1, :b, :south)
       {:ok, %ToyRobot.Position{facing: :south, x: 1, y: :b}}
-
       iex> ToyRobot.place(-1, :f, :north)
       {:failure, "Invalid position"}
-
       iex> ToyRobot.place(3, :c, :north_east)
       {:failure, "Invalid facing direction"}
   """
@@ -52,63 +47,111 @@ defmodule ToyRobot do
   end
 
 
-  def move_to(n,robot,cli_proc_name) when n <= 0 do
-    robot
+  def move_to(n,robot,cli_proc_name, obs_list, element_list) when n <= 0 do
+    node  = %ToyRobot{x: robot.x,y: @robot_map_y_atom_to_num[robot.y], facing: robot.facing}
+    [robot, obs_list, element_list] = if(!Enum.member?(element_list, node)) do
+      element_list = element_list ++ [node]
+      is_obs = send_robot_status(robot,cli_proc_name)
+
+      [robot, obs_list, element_list] = if(is_obs) do
+        obs_list = if(!Enum.member?(obs_list, node)) do
+          obs_list = obs_list ++ [node]
+        else
+          obs_list
+        end
+        [robot, obs_list, element_list]
+      else
+        [robot, obs_list, element_list]
+      end
+    else
+    [robot, obs_list, element_list]
+    end
+    [robot, obs_list, element_list]
   end
 
-  def move_to(n,robot,cli_proc_name)do
-    send_robot_status(robot,cli_proc_name)
+  def move_to(n,robot,cli_proc_name, obs_list, element_list)do
+
+    node  = %ToyRobot{x: robot.x,y: @robot_map_y_atom_to_num[robot.y], facing: robot.facing}
+
+    [robot, obs_list, element_list] = if(!Enum.member?(element_list, node)) do
+      element_list = element_list ++ [node]
+
+      is_obs = send_robot_status(robot,cli_proc_name)
+
+      [robot, obs_list, element_list] = if(is_obs) do
+        obs_list = if(!Enum.member?(obs_list, node)) do
+          obs_list = obs_list ++ [node]
+        else
+          obs_list
+        end
+        move_to(n-1,robot,cli_proc_name, obs_list, element_list)
+        #break
+      else
+      robot = move(robot)
+      move_to(n-1,robot,cli_proc_name, obs_list, element_list)
+      end
+  else
     robot = move(robot)
-    move_to(n-1,robot,cli_proc_name)
+    move_to(n-1,robot,cli_proc_name, obs_list, element_list)
+  end
+    [robot, obs_list, element_list]
   end
 
 
   #loop for orientation
-  def rotate(robot, required) when robot.facing == required do
-      # IO.puts(robot.facing)
-      robot
-  end
-
-  def rotate(robot, required) do
-      robot = left(robot)
-      robot = rotate(robot, required)
-  end
-
-
-  def change_orientation_x(robot,error_x) do
-
-    robot = if(error_x>0) do
-      rotate(robot, :east)
-    else
-      robot
-    end
-
-    robot = if(error_x<0) do
-      rotate(robot, :west)
-    else
-      robot
-    end
-
+  def rotate(robot, required, cli_proc_name) when robot.facing == required do
+    # IO.puts(robot.facing)
     robot
   end
 
-  def change_orientation_y(robot,error_y) do
+  def rotate(robot, required, cli_proc_name) do
+  robot = if(required==@directions_to_the_right[robot.facing]) do
+    robot = right(robot)
+    # send_robot_status(robot, cli_proc_name)
+    robot
 
-    robot = if(error_y>0) do
-      rotate(robot, :north)
-    else
-      robot
-    end
+  else
+    robot = left(robot)
+    # send_robot_status(robot, cli_proc_name)
+    robot
+  end
+    rotate(robot, required, cli_proc_name)
+end
 
-    robot = if(error_y<0) do
-      rotate(robot, :south)
-    else
-      robot
-    end
 
+def change_orientation_x(robot,error_x, cli_proc_name) do
+
+  robot = if(error_x>0) do
+    rotate(robot, :east, cli_proc_name)
+  else
     robot
   end
 
+  robot = if(error_x<0) do
+    rotate(robot, :west, cli_proc_name)
+  else
+    robot
+  end
+
+  robot
+end
+
+def change_orientation_y(robot,error_y, cli_proc_name) do
+
+  robot = if(error_y>0) do
+    rotate(robot, :north, cli_proc_name)
+  else
+    robot
+  end
+
+  robot = if(error_y<0) do
+    rotate(robot, :south, cli_proc_name)
+  else
+    robot
+  end
+
+  robot
+end
 
   @doc """
   Provide START position to the robot as given location of (x, y, facing) and place it.
@@ -120,37 +163,41 @@ defmodule ToyRobot do
   end
 
 
-  def go_to_point(robot,goal_x, goal_y,cli_proc_name) do
+
+  def go_to_point(robot,goal_x, goal_y,cli_proc_name,obs_list, element_list) do
 
     #IO.inspect robot ,label: "robot is "
 
     error_x = goal_x-robot.x
     error_y = goal_y-@robot_map_y_atom_to_num[robot.y]
 
-  robot = if(error_y != 0) do
-    robot = change_orientation_y(robot, error_y)
+    [robot, obs_list, element_list] = if(error_y != 0) do
+    robot = change_orientation_y(robot, error_y, cli_proc_name)
 
-    robot = if(error_y<0) do
-      move_to(-1*error_y,robot,cli_proc_name)
+    if(error_y<0) do
+      move_to(-1*error_y,robot,cli_proc_name, obs_list, element_list)
     else
-      move_to(error_y,robot,cli_proc_name)
+      move_to(error_y,robot,cli_proc_name, obs_list, element_list)
     end
-
   else
+    [robot, obs_list, element_list]
+  end
 
-    robot = change_orientation_x(robot, error_x)
-    robot = if(error_x<0) do
-      move_to(-1*error_x,robot,cli_proc_name)
+  [robot, obs_list, element_list] = if(error_x != 0) do
+
+    robot = change_orientation_x(robot, error_x, cli_proc_name)
+
+    if(error_x<0) do
+      move_to(-1*error_x,robot,cli_proc_name, obs_list, element_list)
     else
-      move_to(error_x,robot,cli_proc_name)
+      move_to(error_x,robot,cli_proc_name, obs_list, element_list)
     end
+  else
+    [robot, obs_list, element_list]
   end
-    robot
 
-  end
+    [robot,obs_list, element_list]
 
-  def obstacle_found(is_obstacle) do
-    IO.puts(is_obstacle)
   end
 
 
@@ -184,16 +231,30 @@ defmodule ToyRobot do
   end
 
 
-  def getNeighbours(node1) do
+  def getNeighbours(node1, obs_list) do
     #IO.puts "Get neighbours called
     neighbours = []
 
-    #call server -true ,false
+    is_not_walkbale_north = Enum.member?(obs_list,%ToyRobot{x: node1.x, y: node1.y, facing: :north} )
 
+    is_not_walkbale_south = Enum.member?(obs_list,%ToyRobot{x: node1.x, y: node1.y, facing: :south} )
+
+    is_not_walkbale_east = Enum.member?(obs_list,%ToyRobot{x: node1.x, y: node1.y, facing: :east} )
+
+    is_not_walkbale_west = Enum.member?(obs_list,%ToyRobot{x: node1.x, y: node1.y, facing: :west} )
+
+
+
+
+    # IO.inspect is_not_walkbale_north,label: "Node is"
     #north
     temp = node1.y + 1
     neighbours = if (node1.x > 0 and node1.x <= 5 and temp > 0 and temp <= 5) do
-      neighbours ++ [%ToyRobot{x: node1.x, y: temp}]
+      neighbours = if(is_not_walkbale_north)do
+        neighbours
+      else
+        neighbours ++ [%ToyRobot{x: node1.x, y: temp}]
+      end
     else
       neighbours
     end
@@ -201,7 +262,11 @@ defmodule ToyRobot do
     #south
     temp = node1.y - 1
     neighbours = if (node1.x > 0 and node1.x <= 5 and temp > 0 and temp <= 5) do
-      neighbours = neighbours ++ [%ToyRobot{x: node1.x, y: temp}]
+      neighbours = if(is_not_walkbale_south)do
+        neighbours
+      else
+        neighbours ++ [%ToyRobot{x: node1.x, y: temp}]
+      end
     else
       neighbours
     end
@@ -209,7 +274,12 @@ defmodule ToyRobot do
     #east
     temp = node1.x + 1
     neighbours = if (node1.y > 0 and node1.y <= 5 and temp > 0 and temp <= 5) do
-      neighbours ++ [%ToyRobot{x: temp, y: node1.y}]
+      neighbours = if(is_not_walkbale_east)do
+        neighbours
+      else
+        neighbours ++ [%ToyRobot{x: temp, y: node1.y}]
+      end
+
     else
       neighbours
     end
@@ -217,11 +287,16 @@ defmodule ToyRobot do
     #west
     temp = node1.x - 1
     neighbours = if (node1.y > 0 and node1.y <= 5 and temp > 0 and temp <= 5) do
-      neighbours ++ [%ToyRobot{x: temp, y: node1.y}]
+      neighbours = if(is_not_walkbale_west)do
+        neighbours
+      else
+        neighbours ++ [%ToyRobot{x: temp, y: node1.y}]
+      end
     else
       neighbours
     end
 
+    # IO.inspect obs_list ,label: "obstacles is "
     {:ok,neighbours}
   end
 
@@ -239,17 +314,16 @@ defmodule ToyRobot do
   end
 
 
-  @spec astar_inner_loop_2(any, any, any, any, any, any, any) :: {:ok, [...]}
-  def astar_inner_loop_2(x, n, neighbours, closedset, openset, node1, targetNode) when x==n do
+  def astar_inner_loop_2(x, n, neighbours, closedset, openset, node1, targetNode,obs_list) when x==n do
     {:ok,[ neighbours, closedset, openset, node1]}
   end
 
-  def astar_inner_loop_2(x, n, neighbours, closedset, openset, node1, targetNode) do
+  def astar_inner_loop_2(x, n, neighbours, closedset, openset, node1, targetNode,obs_list) do
         #IO.puts "Astar inner loop 2"
         currentNode = Enum.at(neighbours,x)
 
-				if (!currentNode.walkable or Enum.member?(closedset, currentNode)) do
-					astar_inner_loop_2(x+1, n, neighbours, closedset, openset, node1, targetNode)
+				if (Enum.member?(closedset, currentNode)) do
+					astar_inner_loop_2(x+1, n, neighbours, closedset, openset, node1, targetNode, obs_list)
 
         else
 
@@ -264,13 +338,13 @@ defmodule ToyRobot do
 						openset ++ [currentNode]
           end
         end
-        {:ok,[ neighbours, closedset, openset, node1]} = astar_inner_loop_2(x+1, n, neighbours, closedset, openset, node1, targetNode)
+        {:ok,[ neighbours, closedset, openset, node1]} = astar_inner_loop_2(x+1, n, neighbours, closedset, openset, node1, targetNode, obs_list)
       end
   end
 
 
 
-  def astar_inner_loop(openset,closedset,startNode, node1,targetNode, x, n) when x==n do
+  def astar_inner_loop(openset,closedset,startNode, node1,targetNode, x, n,obs_list) when x==n do
     openset = List.delete(openset, node1)
     closedset = closedset ++ [node1]
     completed = false
@@ -296,7 +370,7 @@ defmodule ToyRobot do
 
   end
 
-  def astar_inner_loop(openset, closedset,startNode, node1,targetNode, x, n) do
+  def astar_inner_loop(openset, closedset,startNode, node1,targetNode, x, n,obs_list) do
 
     #IO.inspect node1, label: "this is node1 inner loop"
 
@@ -314,16 +388,16 @@ defmodule ToyRobot do
       node1
     end
     # IO.inspect openset, label: "The openset is"
-    {:ok,[path, node1,openset,closedset,completed]} = astar_inner_loop(openset,closedset, startNode, node1,targetNode, x+1, n)
+    {:ok,[path, node1,openset,closedset,completed]} = astar_inner_loop(openset,closedset, startNode, node1,targetNode, x+1, n, obs_list)
 
   end
 
 
-  def astar_loop1(n, openset, closedset, x_goal, y_goal, startNode,path) when n<1 do
+  def astar_loop1(n, openset, closedset, x_goal, y_goal, startNode,path,obs_list) when n<1 do
     path
   end
 
-  def astar_loop1(n, openset, closedset, x_goal, y_goal, startNode,path) do
+  def astar_loop1(n, openset, closedset, x_goal, y_goal, startNode,path,obs_list) do
 
     node1 = hd(openset)
     #IO.inspect node1, label: "this is node1"
@@ -331,33 +405,127 @@ defmodule ToyRobot do
 
     #IO.puts "Astar loop1"
 
-    {:ok,[path, node1,openset,closedset,completed]} = astar_inner_loop(openset,closedset,startNode, node1,targetNode, 1, length(openset))
+    {:ok,[path, node1,openset,closedset,completed]} = astar_inner_loop(openset,closedset,startNode, node1,targetNode, 1, length(openset), obs_list)
 
     path = if(completed) do
-      astar_loop1(0, openset, closedset, x_goal, y_goal, startNode,path)
+      astar_loop1(0, openset, closedset, x_goal, y_goal, startNode,path, obs_list)
     else
 
-    {:ok,neighbours}  = getNeighbours(node1)
-    {:ok,[ neighbours, closedset, openset, node1]} = astar_inner_loop_2(0, length(neighbours), neighbours, closedset, openset, node1, targetNode)
+    {:ok,neighbours}  = getNeighbours(node1, obs_list)
+    {:ok,[ neighbours, closedset, openset, node1]} = astar_inner_loop_2(0, length(neighbours), neighbours, closedset, openset, node1, targetNode, obs_list)
 
-    astar_loop1(length(openset), openset, closedset, x_goal, y_goal, startNode,path)
+    astar_loop1(length(openset), openset, closedset, x_goal, y_goal, startNode,path, obs_list)
     end
   end
 
 
-  def traverse_path_nodes(x, n, robot,cli_proc_name,path) when x==n do
+  def starting_rotate(robot, required, cli_proc_name) when robot.facing == required do
+    # send_robot_status(robot, cli_proc_name)
     robot
   end
 
-  def traverse_path_nodes(x, n, robot,cli_proc_name,path) do
+  def starting_rotate(robot, required, cli_proc_name) do
+  robot = if(required==@directions_to_the_right[robot.facing]) do
+    send_robot_status(robot, cli_proc_name)
+    robot = right(robot)
+    robot
 
-    currentNode = Enum.at(path,x)
+  else
+    send_robot_status(robot, cli_proc_name)
+    robot = left(robot)
+    robot
+  end
+    starting_rotate(robot, required, cli_proc_name)
+end
 
-    robot = go_to_point(robot,currentNode.x,currentNode.y,cli_proc_name)
 
-    traverse_path_nodes(x+1, n, robot,cli_proc_name,path)
+def start_change_orientation_x(robot,error_x, cli_proc_name) do
+
+  robot = if(error_x>0) do
+    starting_rotate(robot, :east, cli_proc_name)
+  else
+    robot
   end
 
+  robot = if(error_x<0) do
+    starting_rotate(robot, :west, cli_proc_name)
+  else
+    robot
+  end
+
+  robot
+end
+
+def start_change_orientation_y(robot,error_y, cli_proc_name) do
+
+  robot = if(error_y>0) do
+    starting_rotate(robot, :north, cli_proc_name)
+  else
+    robot
+  end
+
+  robot = if(error_y<0) do
+    starting_rotate(robot, :south, cli_proc_name)
+  else
+    robot
+  end
+
+  robot
+end
+
+
+  def traverse_path_nodes(x, n, robot,cli_proc_name,path,obs_list, element_list) when x==n do
+    [robot,obs_list, element_list]
+  end
+
+  def traverse_path_nodes(x, n, robot,cli_proc_name,path,obs_list, element_list) do
+
+    neighbourNode = Enum.at(path,x)
+
+    robot = if(x==0 and length(element_list)==0) do
+      error_x = neighbourNode.x-robot.x
+      error_y = neighbourNode.y-@robot_map_y_atom_to_num[robot.y]
+      robot = start_change_orientation_x(robot, error_x, cli_proc_name)
+      robot = start_change_orientation_y(robot, error_y, cli_proc_name)
+    else
+      robot
+    end
+
+    [robot,obs_list_new, element_list] = go_to_point(robot,neighbourNode.x,neighbourNode.y,cli_proc_name,obs_list, element_list)
+
+    # IO.inspect obs_list_new, label: "new obstacle list is"
+    # IO.inspect robot, label: "new robot is"
+    if(length(obs_list_new) != length(obs_list)) do  #break
+      traverse_path_nodes(1, 1, robot,cli_proc_name,path,obs_list_new, element_list)
+    else
+      traverse_path_nodes(x+1, n, robot,cli_proc_name,path,obs_list, element_list)
+    end
+  end
+
+
+def path_planner(robot ,goal_x,goal_y,cli_proc_name,obs_list,parent, element_list) when (robot.x==goal_x and robot.y==goal_y) do
+  # send_robot_status(robot,cli_proc_name)
+  if(length(element_list)==0) do
+  send_robot_status(robot, cli_proc_name)
+  end
+  send(parent,{:put,robot})
+  #save final robot position
+
+end
+
+def path_planner(robot,goal_x,goal_y,cli_proc_name,obs_list,parent, element_list) do
+  startNode = %ToyRobot{x: robot.x, y: @robot_map_y_atom_to_num[robot.y]}
+  path = []
+  openset = []
+  closedset = []
+  openset = openset ++ [startNode]
+
+  path = astar_loop1(length(openset), openset, closedset, goal_x, goal_y,startNode,path,obs_list)
+
+  [robot,obs_list, element_list] = traverse_path_nodes(0, length(path),robot,cli_proc_name,path,obs_list, element_list)
+
+  path_planner(robot,goal_x,goal_y,cli_proc_name,obs_list,parent, element_list)
+end
 
 
 
@@ -372,22 +540,18 @@ defmodule ToyRobot do
 
   def stop(robot, goal_x, goal_y, cli_proc_name) do
 
-    Process.register(spawn_link(fn -> listen_from_server() end), :client_toyrobot)
+    obs_list = []
+    element_list = []
+    parent = self()
+    Process.register(spawn_link(fn -> path_planner(robot,goal_x,goal_y,cli_proc_name,obs_list,parent, element_list) end), :client_toyrobot)
 
-    #go_to_point(robot, goal_x, goal_y,cli_proc_name)
+    #get final robot position
+    robot = receive do
+      {:put,robot} ->
+        robot
+    end
 
-    startNode = %ToyRobot{x: robot.x, y: @robot_map_y_atom_to_num[robot.y]}
-    path = []
-    openset = []
-    closedset = []
-
-    openset = openset ++ [startNode]
-
-    path = astar_loop1(length(openset), openset, closedset, goal_x, goal_y,startNode,path)
-
-    #IO.inspect path,label: "The path is"
-
-    robot = traverse_path_nodes(0, length(path),robot,cli_proc_name,path)
+    {:ok,robot}
 
   end
 
@@ -396,8 +560,9 @@ defmodule ToyRobot do
   to the CLI Server process after each action is taken.
   """
   def send_robot_status(%ToyRobot.Position{x: x, y: y, facing: facing} = _robot, cli_proc_name) do
-    send(cli_proc_name,{:toyrobot_status, x, y, facing})
+    send(cli_proc_name, {:toyrobot_status, x, y, facing})
     # IO.puts("Sent by Toy Robot Client: #{x}, #{y}, #{facing}")
+    listen_from_server()
   end
 
   @doc """
@@ -406,20 +571,13 @@ defmodule ToyRobot do
   """
   def listen_from_server() do
     receive do
-      {:obstacle_presence, is_obs_ahead} ->
-        if(is_obs_ahead) do
-          obstacle_found(is_obs_ahead)
-        end
-        is_obs_ahead
+      {:obstacle_presence, is_obs_ahead} -> is_obs_ahead
     end
-    listen_from_server()
   end
 
   @doc """
   Provides the report of the robot's current position
-
   Examples:
-
       iex> {:ok, robot} = ToyRobot.place(2, :b, :west)
       iex> ToyRobot.report(robot)
       {2, :b, :west}
@@ -428,7 +586,7 @@ defmodule ToyRobot do
     {x, y, facing}
   end
 
-  @directions_to_the_right %{north: :east, east: :south, south: :west, west: :north}
+
   @doc """
   Rotates the robot to the right.
   """
